d749d7a8eb2d50969932f89ebfb52c9aef11a1dd
Kola771 <koladeaboudou@gmail.com>
2023-09-08 13:55:39 +0200

    chat

--- Public/assets/js/forget.js
+++ Public/assets/js/forget.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let email = document.getElementById("email");
     let email0 = document.querySelector(".email0");
     let code = document.querySelector(".code");
+    let codevalide = document.getElementById("code");
     let pass1 = document.querySelector(".pass1");
     let pass2 = document.querySelector(".pass2");
     email.addEventListener("keyup", (e) => {
@@ -29,5 +30,31 @@ document.addEventListener("DOMContentLoaded", () => {
                 code.classList.remove("hidden");
             }
         })
+    })
+    code.addEventListener("keyup", (e) => {
+        let data = {
+            code : codevalide.value
+        }
+        data = JSON.stringify(data);
+        let options = {
+            header : {
+                "content" : "application/json"
+            }, 
+            body: data,
+            method: "post"
+        }
+        console.log(data);
+
+        // fetch("/register-controller/verify-email-inp", options).then(response => response.json()).then(response => {
+        //     if(response.error)
+        //     {
+        //         paragraph.innerHTML = response.error;
+        //         paragraph.classList.remove("hidden");
+        //     } else {
+        //         paragraph.classList.add("hidden");
+        //         email0.classList.add("hidden");
+        //         code.classList.remove("hidden");
+        //     }
+        // })
     })
 })
